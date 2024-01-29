02cd102f13b22c38a0a04482688e36cd56de4c62
Bhargav B L <blbhargav@envistacorp.com>
2024-01-29 15:16:26 +0530

    Initial commit

---
+++ android/src/main/java/com/koerber/deviceinfo/DeviceInfo.java
@@ -0,0 +1,11 @@
+package com.koerber.deviceinfo;
+
+import android.util.Log;
+
+public class DeviceInfo {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
